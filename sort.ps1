9ba183740cb9b57d5975b0e2910df98768d71e0d
Saulotas <Saulotas@users.noreply.github.com>
2020-09-25 08:09:38 -0400

    Update sort.ps1

--- sort.ps1
+++ sort.ps1
@@ -1,22 +1,23 @@
 $location = Get-Location
-$files = Get-ChildItem -LiteralPath $location -Force -name -file -exclude *.ps1, *.exe
+$files = Get-ChildItem  $location -Force -name -file  -exclude *.ps1, *.exe
 
 $dirnames = $(foreach($file in $files){$file.Substring(0,1).toupper()})| Sort-Object | Get-Unique
 #Write-Output ($dirnames)
 foreach($dir in $dirnames)
 {
-if (-not (Test-Path -LiteralPath $dir)) {
-
-if($dir -Match '[0-9]|\[') {$dir = '#'}
-New-Item -Path "$location\export\$dir" -ItemType Directory
-
+    if (-not (Test-Path -LiteralPath $location\export\#) -And ($dir -Match '[0-9]|\[')) 
+        {New-Item -Path "$location\export\#" -ItemType Directory}
+    Elseif (-not ($dir -Match '[0-9]|\['))
+    {
+        New-Item -Path "$location\export\$dir" -ItemType Directory
+    }
 }
-}
+
 
 foreach($file in $files)
 {
-$direct = $file.Substring(0,1).toupper()
-if($direct -Match '[0-9]|\[') {$direct = '#'}
-Move-Item -LiteralPath $location\$file -Destination $location\export\$direct\$file
+    $direct = $file.Substring(0,1).toupper()
+    if($direct -Match '[0-9]|\[') {$direct = '#'}
+    Move-Item -LiteralPath $location\$file -Destination $location\export\$direct\$file
 }
 
