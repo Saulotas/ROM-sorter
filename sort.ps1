2ae7cd447113eee38bf509e6e491c3a9893f630d
Saulotas <Saulotas@users.noreply.github.com>
2020-09-25 07:29:46 -0400

    Add files via upload

---
+++ sort.ps1
@@ -0,0 +1,22 @@
+﻿$location = Get-Location
+$files = Get-ChildItem -LiteralPath $location -Force -name -file -exclude *.ps1,*.log
+
+$dirnames = $(foreach($file in $files){$file.Substring(0,1).toupper()})| Sort-Object | Get-Unique
+#Write-Output ($dirnames)
+foreach($dir in $dirnames)
+{
+if($dir -Match '[0-9]|\[') {$dir = '#'}
+New-Item -Path "$location\export\$dir" -ItemType Directory
+}
+
+foreach($file in $files)
+{
+$direct = $file.Substring(0,1).toupper()
+if($direct -Match '[0-9]|\[') {$direct = '#'}
+Move-Item -LiteralPath $location\$file -Destination $location\export\$direct\$file
+}
+
+"$files"| out-file -filepath $location\list.log
+
+
+
