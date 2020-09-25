fe973edf6e9268c417aa288ce9e9a86982cda6cd
Saulotas <Saulotas@users.noreply.github.com>
2020-09-25 07:34:20 -0400

    Update sort.ps1

--- sort.ps1
+++ sort.ps1
@@ -1,12 +1,16 @@
-﻿$location = Get-Location
-$files = Get-ChildItem -LiteralPath $location -Force -name -file -exclude *.ps1,*.log
+$location = Get-Location
+$files = Get-ChildItem -LiteralPath $location -Force -name -file -exclude *.ps1, *.exe
 
 $dirnames = $(foreach($file in $files){$file.Substring(0,1).toupper()})| Sort-Object | Get-Unique
 #Write-Output ($dirnames)
 foreach($dir in $dirnames)
 {
+if (-not (Test-Path -LiteralPath $dir)) {
+
 if($dir -Match '[0-9]|\[') {$dir = '#'}
 New-Item -Path "$location\export\$dir" -ItemType Directory
+
+}
 }
 
 foreach($file in $files)
@@ -15,8 +19,4 @@ $direct = $file.Substring(0,1).toupper()
 if($direct -Match '[0-9]|\[') {$direct = '#'}
 Move-Item -LiteralPath $location\$file -Destination $location\export\$direct\$file
 }
-
-"$files"| out-file -filepath $location\list.log
-
-
 
